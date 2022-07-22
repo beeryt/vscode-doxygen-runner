b8518ace44d9f73d200a65a71e8e2df1a903767c
Carl B. Smiley <beeryt@users.noreply.github.com>
2022-07-21 23:59:05 -0700

    Add option for generating doxygen on save

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,15 @@ let instance_manager: InstanceManager;
 export function activate(context: vscode.ExtensionContext) {
   instance_manager = new InstanceManager(context);
 
+  let config = vscode.workspace.getConfiguration('doxygen_runner');
+
+  if (config['generate_on_save'])
+  vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+    if (document.uri.scheme === 'file') {
+      vscode.commands.executeCommand('extension.doxygen-runner.generate_doxygen', this.basedir);
+    }
+  })
+
   context.subscriptions.push(
     vscode.commands.registerCommand('extension.doxygen-runner.generate_doxygen', (filepath) => {
       try {
